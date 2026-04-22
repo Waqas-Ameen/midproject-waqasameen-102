79148f2eb8090a121cdeade92b12752b97649475
Waqas Ameen <waqasameen10674@gmail.com>
2026-04-22 15:17:54 +0500

    Delete Project directory

--- Project/src/pages/CityPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-
-const CityPage = () => {
-  const { city } = useParams();
-  return <div>Ads in city: {city}</div>;
-};
-
-export default CityPage;
